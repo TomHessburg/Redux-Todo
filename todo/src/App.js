cc81462b344f43b44798b69769a407fd50774c22
Tom Hessburg <tomhessburg@Toms-MacBook-Pro.local>
2019-03-04 18:44:29 -0500

    set up project

---
+++ todo/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <h1>Redux Todo</h1>
+       <div>
+         <h2>our todos will go here...</h2>
+       </div>
+      </div>
+    );
+  }
+}
+
+export default App;
